7b55acbec66064038faf52fbe8a17e797397eab8
yuhuUSTC <55074512+yuhuUSTC@users.noreply.github.com>
2023-07-28 10:55:29 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+optpath='FSDGN/basicsr/options/test/dehaze_test.yml'
+export PYTHONPATH=/bfs/yuhu/code/FSDGN:$PYTHONPATH
+CUDA_VISIBLE_DEVICES="2" python /bfs/yuhu/code/FSDGN/basicsr/test.py -opt $optpath
